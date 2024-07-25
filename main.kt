8ba88a99051369b0ff6c5a5f3ffc9e5141b55189
sigmaiosdeveloper <sigmaiosdeveloper1234@gmail.com>
2024-07-25 14:40:53 -0700

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,11 @@
+class hacktheplanet {
+  val why = "i want to"
+  fun hackit() {
+    println("hacking successful")
+  }
+}
+fun main() {
+  var hacker = hacktheplanet()
+  println(hacker.why)
+  hackit()
+}
